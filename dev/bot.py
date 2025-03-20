08f4826905d5318e0bfbf9deb686d00f1abf56d8
Nikonorov.M <maximnikonorov957@gmail.com>
2025-03-20 14:21:36 +0300

    Добавлены новые загадки АЯ по соответственным сложностям, добавлена кнопка back и ее соответственный функционал.

--- dev/bot.py
+++ dev/bot.py
@@ -1,7 +1,8 @@
 import telebot
 import config
-from start import handle_start, answer_from_user_1, answer_from_user_2, level_learning_p2
+from start import handle_start, answer_from_user_1, answer_from_user_2, level_learning_p2, answer_from_user_3
 
+# Инициализация бота
 bot = telebot.TeleBot(config.TOKEN)
 
 # Вызов функций для установки обработчиков
@@ -9,6 +10,8 @@ handle_start(bot)
 answer_from_user_1(bot)
 answer_from_user_2(bot)
 level_learning_p2(bot)
+answer_from_user_3(bot)
 
+# Запуск бота
 if __name__ == "__main__":
     bot.polling(none_stop=True)
